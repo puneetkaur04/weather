f2d2f0a37bab78fea587b87e746346feaca1af83
Puneet Kaur <pkaur157@west-mec.org>
2023-04-26 14:17:21 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const api = {
+    key: "d45bdc758224238408ebc5e00d9fb567",
+    url: "https://api.openweathermap.org/data/2.5/"
+  }
 }
 
 export default App;
