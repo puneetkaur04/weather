6f91e2cd7e59f304b635f9b759bb5a957c88b0d1
Puneet Kaur <pkaur157@west-mec.org>
2023-04-26 14:28:19 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
-import './App.css';
-
-
 function App() {
   const api = {
     key: "d45bdc758224238408ebc5e00d9fb567",
     url: "https://api.openweathermap.org/data/2.5/"
   }
+  return (
+    
+  )
 }
 
 export default App;
